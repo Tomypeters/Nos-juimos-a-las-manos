9f4dcc0108d35d44497116a5930208154534af39
Tomypeters <tomypeters@hotmail.com>
2022-11-20 12:33:29 -0300

    Credits

--- Assets/TomiScripts/SceneManagement.cs
+++ Assets/TomiScripts/SceneManagement.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void ChangeScene(string scene) 
     {
-        
+        SceneManager.LoadScene(scene);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TransicionACreditos() 
     {
-        
+        ChangeScene("Credits");
     }
 }
